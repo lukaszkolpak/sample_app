be6d7c2185789abe3e384f20de19d661c1c1ed3d
lukaszkolpak <lukasz.kolpak@netguru.pl>
2018-03-09 10:48:44 +0100

    Adds S3 region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAIPI4F6HLTUWBGEVA'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region => ENV[’S3_REGION’]
     }
-    config.fog_directory     =  ENV['bucket1testasd']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
